f9d8505ab452a948b417dee740915b4b980bccaa
Tristan Leo <vileda@vileda.cc>
2023-01-29 22:18:36 +0100

    ansible vault

---
+++ src/core.rs
@@ -0,0 +1,26 @@
+use derive_more::Display;
+use inquire::error::InquireResult;
+use inquire::Select;
+
+#[derive(Display)]
+#[display(fmt = "{}", prompt)]
+pub struct Choice<T> where T: std::fmt::Display {
+    pub(crate) choice: T,
+    pub(crate) prompt: &'static str,
+}
+
+#[derive(Display)]
+pub enum Action {
+    Generate,
+    Import,
+}
+
+#[derive(Display)]
+pub enum Chores {
+    VaultSecret,
+    Service,
+}
+
+pub fn prompt<T>(prompt: &str, choices: Vec<Choice<T>>) -> InquireResult<Choice<T>> where T: std::fmt::Display {
+    Select::new(prompt, choices).prompt()
+}
---
+++ src/vault.rs
@@ -0,0 +1,137 @@
+use std::collections::BTreeMap;
+use std::path::Path;
+use std::{fs, str};
+use std::process::Command;
+use ansible_vault::decrypt_vault_from_file;
+use base64::Engine;
+use inquire::{Password, PasswordDisplayMode, Text};
+use rand_chacha::ChaCha20Rng;
+use rand_chacha::rand_core::{RngCore, SeedableRng};
+use regex::Regex;
+use crate::completer::FilePathCompleter;
+use crate::core::{Choice, prompt};
+use crate::core::Action::{Generate, Import};
+
+pub fn handle_vault_secret() {
+    let vault_password = if std::env::var("ANSIBLE_VAULT_PASSWORD").is_err() {
+        Password::new("Ansible vault password:")
+            .with_display_mode(PasswordDisplayMode::Masked)
+            .prompt()
+            .expect("Failed to get vault password")
+    } else {
+        get_vault_password()
+    };
+
+    if vault_password.len() < 12 {
+        panic!("The vault password must be at least 12 characters long");
+    }
+
+    match prompt("Do you want to generate a new secret?", vec![
+        Choice { choice: Generate, prompt: "Generate a new secret" },
+        Choice { choice: Import, prompt: "Import a secret" },
+    ]) {
+        Ok(choice) => {
+            match choice.choice {
+                Generate => handle_vault_secret_generate(vault_password),
+                Import => handle_vault_secret_import(vault_password),
+            }
+        }
+        Err(_) => println!("There was an error, please try again"),
+    };
+}
+
+fn prompt_secret_name() -> String {
+    let name = Text::new("What is the name of the secret?").prompt();
+
+    name.map(|name| {
+        let re = Regex::new(r"[^A-Za-z0-9]").unwrap();
+        let name = re.replace_all(&name.trim().to_string(), "_").to_string();
+        if name.starts_with("vault_") { name } else { format!("vault_{}", name) }
+    }).expect("Failed to get secret name")
+}
+
+fn handle_vault_secret_import(vault_password: String) {
+    let secret_name = prompt_secret_name();
+    let secret = Password::new("The secret text").prompt()
+        .expect("Failed to get secret text");
+
+    add_vault_secret(vault_password, secret_name, secret);
+}
+
+fn handle_vault_secret_generate(vault_password: String) {
+    let secret_name = prompt_secret_name();
+    let secret = generate_secret();
+
+    add_vault_secret(vault_password, secret_name, secret);
+}
+
+fn add_vault_secret(vault_password: String, secret_name: String, secret: String) {
+    let vault_file_path = prompt_vault_file_path();
+    if !Path::new(vault_file_path.as_str()).exists() {
+        create_vault_file(vault_file_path.as_str(), vault_password.clone());
+    }
+    let absolute_vault_file_path = fs::canonicalize(vault_file_path.clone()).unwrap();
+    let absolute_vault_file_path = absolute_vault_file_path.to_str().unwrap();
+
+    add_vault_secret_to_file(secret_name, secret, absolute_vault_file_path, vault_password);
+}
+
+fn generate_secret() -> String {
+    print!("Generating a new secret...");
+    let mut rng = ChaCha20Rng::from_entropy();
+    let mut bytes = [0u8; 32];
+    rng.fill_bytes(&mut bytes);
+    let secret = base64::engine::general_purpose::STANDARD.encode(&bytes);
+    println!("done");
+    secret
+}
+
+fn prompt_vault_file_path() -> String {
+    let vault_file_path = Text::new("Where is the vault file located? (tab to autocomplete)")
+        .with_autocomplete(FilePathCompleter::default())
+        .prompt()
+        .expect("Failed to get vault file path");
+    if vault_file_path.starts_with("./") {
+        vault_file_path
+    } else {
+        "./".to_string() + &*vault_file_path
+    }
+}
+
+fn add_vault_secret_to_file(secret_name: String, secret: String, vault_file_path: &str, password: String) {
+    let mut vault_file = decrypt_vault_file(vault_file_path, password.clone());
+    vault_file.insert(secret_name, secret);
+
+    let vault_file_string = serde_yaml::to_string(&vault_file).unwrap();
+    let encrypted = ansible_vault::encrypt_vault(vault_file_string.as_bytes(), password.as_str())
+        .expect("Failed to encrypt vault");
+
+    fs::write(vault_file_path, encrypted).expect("Failed to write vault file");
+}
+
+fn decrypt_vault_file(file: &str, password: String) -> BTreeMap<String, String> {
+    let decrypted = decrypt_vault_from_file(file, password.as_str()).unwrap();
+    serde_yaml::from_str(str::from_utf8(&decrypted).expect("UTF-8 content expected"))
+        .expect("Failed to parse decrypted vault file")
+}
+
+fn create_vault_file(file: &str, password: String) {
+    execute_command(format!("ansible-vault create {file} --vault-password-file <(cat <<<'{password}')"));
+}
+
+fn get_vault_password() -> String {
+    let vault_password_file = std::env::var("ANSIBLE_VAULT_PASSWORD_FILE").unwrap();
+    execute_command(vault_password_file)
+}
+
+fn execute_command(vault_command: String) -> String {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(vault_command.clone())
+        .output()
+        .expect("failed to execute process");
+    if !output.status.success() {
+        panic!("Failed to execute command: {}\n{}", vault_command, String::from_utf8_lossy(&output.stderr));
+    }
+    String::from_utf8_lossy(&output.stdout).to_string()
+}
