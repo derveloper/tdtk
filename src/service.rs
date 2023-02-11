76f986011f9fd2e9c846fa9ec13a30ff315bfda4
Tristan Leo <vileda@vileda.cc>
2023-02-11 19:06:00 +0100

    adding dynamic questions for services

--- src/service.rs
+++ src/service.rs
@@ -2,12 +2,45 @@ use std::fs;
 use std::path::Path;
 
 use anyhow::{Context, Result};
+use derive_more::Display;
 use inquire::{Select, Text};
 use oauth2::TokenResponse;
 use octocrab::models::User;
+use serde::{Deserialize, Serialize};
 
 use crate::core::execute_command;
 use crate::github::get_github_token;
+
+#[derive(Debug, Display, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[display(fmt = "name: {}, question: {}, required: {}, default: {:?}, options: {:?}, condition: {:?}", name, question, required, default, options, condition)]
+struct Question {
+    name: String,
+    question: String,
+    required: bool,
+    default: Option<String>,
+    options: Option<Vec<OptionElement>>,
+    condition: Option<Condition>,
+}
+
+#[derive(Debug, Display, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[display(fmt = "display: {}, value: {:?}", display, value)]
+struct OptionElement {
+    display: String,
+    value: Option<String>,
+}
+
+#[derive(Debug, Display, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[display(fmt = "question: {}, values: {:?}", question, values)]
+struct Condition {
+    question: String,
+    values: Vec<String>,
+}
+
+#[derive(Debug, Display, PartialEq, Eq, Clone, Deserialize, Serialize)]
+#[display(fmt = "questions: {:?}", questions)]
+struct Root {
+    questions: Vec<Question>,
+}
 
 pub async fn handle_service(repo_template: String) -> Result<()> {
     let service_name = Text::new("What is the name of the service?")
@@ -39,6 +72,25 @@ pub async fn handle_service(repo_template: String) -> Result<()> {
     let service_description = Text::new("What is the description of the service?")
         .prompt()
         .with_context(|| "Failed to get service description")?;
+
+    let questions = fs::read_to_string("service-spec.yaml")
+        .with_context(|| "Failed to read service-spec.yaml")?;
+    let questions: Root = serde_yaml::from_str(questions.as_str())
+        .with_context(|| "Failed to parse service-spec.yaml")?;
+
+    for question in questions.questions {
+        if question.options.is_some() && question.clone().options.unwrap().len() > 0 {
+            let options = question.options.unwrap();
+            let options: Vec<String> = options.iter().map(|o| o.display.clone()).collect();
+            let _ = Select::new(question.question.as_str(), options)
+                .prompt()
+                .with_context(|| "Failed to get input")?;
+        } else {
+            let _ = Text::new(question.question.as_str())
+                .prompt()
+                .with_context(|| "Failed to get input")?;
+        }
+    }
 
     let token_response = get_github_token().await?;
 
